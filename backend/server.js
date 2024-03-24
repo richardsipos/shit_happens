c92ebdc20142478a6ab9411d5821b0d659fbe12d
SIRI <richardsipos2003@gmail.com>
2024-03-24 15:44:44 +0100

    Sockets fixed. Lobby creation/join implemented. Disconnecting from lobbies also implemented.

--- backend/server.js
+++ backend/server.js
@@ -14,78 +14,69 @@ const io = new Server(server, {
 
 app.use(cors());
 
-// Object to store lobbies
-
+// Key, users, sockets
+let lobbies = [];
+let playerToDelete = {};
 
 io.on("connection", (socket) => {
-    const lobbies = [];
-    console.log(`User connected: ${socket.id}`);
-
-    socket.on("disconnect", () => {
-        console.log(`User disconnected: ${socket.id}`);
-        Object.keys(lobbies).forEach((channelKey) => {
-          lobbies[channelKey] = lobbies[channelKey].filter(
-            (user) => user.socketId !== socket.id
-          );
-          io.emit("updateUserCount", {
-            channelKey,
-            count: lobbies[channelKey].length,
-          });
-        });
-      });
-    
-
-
-    socket.on("createLobby", async ({ username, channelKey }) => {
-        try {
-          const existingLobby = lobbies.find((lobby) => lobby.channelKey === channelKey);
-          if (existingLobby) {
-            socket.emit("lobbyCreationError", {
-              message: "A lobby with that name already exists. Please choose a different name.",
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+    lobbies.forEach((lobby) => {
+      lobby.players.forEach((player) => {
+        if (player.socket.id === socket.id) {
+          console.log(`Player ${player.username} disconnected from ${lobby.key}`);
+          playerToDelete = player;
+          // Emit updateUserCount to all players in the lobby
+          lobby.players.forEach((lobbyPlayer) => {
+            lobbyPlayer.socket.emit("updateUserCount", {
+              channelKey: lobby.key,
+              count: lobby.players.length - 1, // -1 because the disconnected player is removed
             });
-            return;
-          }
-      
-            // Create the lobby logic here
-
-            if (!lobbies[channelKey]) {
-                lobbies[channelKey] = [];
-            }
-            
-            lobbies[channelKey].push({ username, isCreator: true });
-            console.log("User count in room:",lobbies[channelKey].length)
-            socket.emit("updateUserCount", { channelKey, count: 1 });
-
-
-        } catch (error) {
-          console.error("Error creating lobby:", error);
-          socket.emit("lobbyCreationError", {
-            message: "An error occurred while creating the lobby. Please try again.",
           });
         }
       });
-  
-      socket.on("joinLobby", ({ username, channelKey }) => {
-        console.log(`${username} joined the lobby with channel key ${channelKey}`);
-        if (lobbies[channelKey]) {
-        // Check if lobby already has a user
-        if (lobbies[channelKey].length >= 1) {
-            socket.emit("joinLobbyError", { message: "Lobby is full." });
-            return;
-        }
-        lobbies[channelKey].push({ username, isCreator: false });
-        console.log(lobbies[channelKey]);
-        io.emit("userJoinedLobby", { channelKey, users: lobbies[channelKey] });
-        io.emit("updateUserCount", {
-            channelKey,
-            count: lobbies[channelKey].length,
-        });
-        } else {
-        console.log(`Lobby with channel key ${channelKey} does not exist`);
-        }
     });
+
+    // Remove the disconnected player from the lobby
+    if (playerToDelete) {
+      lobbies.forEach((lobby) => {
+        lobby.players = lobby.players.filter((player) => player !== playerToDelete);
+      });
+    }
   });
-  
+
+  socket.on("createLobby", ({ username, channelKey  }) => {
+    // Check if the channel key already exists
+    if (lobbies.some((lobby) => lobby.key === channelKey)) {
+      console.log("Channel key already exists");
+      return;
+    }
+    console.log("Channel key:"  ,channelKey);
+    console.log("username :"  ,username);
+    //console.log("scoket :"  ,socket);
+    const lobbyToAdd = { key: channelKey, players: [{ username: username, socket: socket }] };
+    lobbies.push(lobbyToAdd);
+    console.log("Lobbies after create:", lobbies);
+  });
+
+  socket.on("joinLobby", ({ username, channelKey }) => {
+    
+    const lobby = lobbies.find((lobby) => lobby.key === channelKey.toString());
+    if (lobby !== undefined) {
+      
+      let player = { username: username, socket: socket }
+      lobby.players.push(player);
+      lobby.players.forEach((player) => {
+        player.socket.emit("updateUserCount", {
+          channelKey: lobby.key,
+          count: lobby.players.length,
+        });
+      });
+    }
+    console.log("Lobbies after join:", lobbies);
+  });
+});
+
 server.listen(8080, () => {
-  console.log('listening on http://localhost:8080');
+  console.log("listening on http://localhost:8080");
 });
